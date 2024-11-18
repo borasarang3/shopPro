07da559e0fab7147eeed57bce093e0a2529f408b
bora <dianabora@naver.com>
2024-11-18 14:12:27 +0900

    쇼핑몰 생성

---
+++ src/main/java/com/example/shoppro/ShopProApplication.java
@@ -0,0 +1,15 @@
+package com.example.shoppro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class ShopProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopProApplication.class, args);
+    }
+
+}
